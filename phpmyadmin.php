c4d4125e47d5612e7d0daa43ca0a029745422f21
Herival <herimalala.val@gmail.com>
2023-11-14 08:51:21 +0100

    ok

--- phpmyadmin.php
+++ phpmyadmin.php
@@ -1,21 +1,32 @@
 <?php 
 # Secure phpmyadmin
-# config.inc.php
+sudo nano /opt/lampp/phpmyadmin/config.inc.php
+# phpmyadmin/config.inc.php
 
 /********************
+ * 
+$cfg['blowfish_secret'] = 'ATUSYEPMLCNAQSURHTFGYVBXDSKJRTPO'; /* YOU SHOULD CHANGE THIS FOR A MORE SECURE COOKIE AUTH! 
+
 $cfg['Servers'][$i]['auth_type'] = 'config';
 $cfg['Servers'][$i]['user'] = 'root';
 $cfg['Servers'][$i]['password'] = '';
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['AllowRoot'] = FALSE;
-/* Server parameters */
+// Server parameters
 //$cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
-/*********************** 
+*********************/
 
 #add password root user
-update user set password=PASSWORD('NEW PASSWORD') where user='root';
- ou
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'nouveau_mot_de_passe';
+sudo /opt/lampp/bin/mysql -u root
+use mysql; 
+
+update user set password=PASSWORD('mot_de_passe') where user='root';
 FLUSH PRIVILEGES;
+
+CREATE USER 'user_admin'@'localhost' IDENTIFIED BY 'Solutions@30';
+GRANT ALL PRIVILEGES ON * . * TO 'user_admin'@'localhost';
+FLUSH PRIVILEGES;
+
+sudo /opt/lampp/lampp restart
